7c7a835c395723af8d29cd62d3c70861f6736621
2MarketingHub <64784186+2marketingHub@users.noreply.github.com>
2020-07-02 12:20:38 +0300

    again

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,13 +37,13 @@ exports.createResolvers = async ({
   })
 }
 
-// const createPosts = require("./create/createPosts")
-// exports.createPagesStatefully = async (
-//   { graphql, actions, reporter },
-//   options
-// ) => {
-//   await createPosts({ actions, graphql, reporter }, options)
-// }
+const createPosts = require("./create/createPosts")
+exports.createPagesStatefully = async (
+  { graphql, actions, reporter },
+  options
+) => {
+  await createPosts({ actions, graphql, reporter }, options)
+}
 
 // category + tag + PAGES
 module.exports.createPages = async ({ graphql, actions }) => {
@@ -175,7 +175,7 @@ module.exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  /*
+  
 
   //categories
   query.data.wpgraphql.categories.edges.forEach(edge => {
@@ -255,7 +255,7 @@ module.exports.createPages = async ({ graphql, actions }) => {
   })
 
 
-  */
+  
  
   // pages
   query.data.wpgraphql.pages.edges.forEach(edge => {
