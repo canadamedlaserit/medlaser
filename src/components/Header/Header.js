1c23ec55bbf1c3539de3612a36e60e4d4b92da0b
EhtashamAli <ehtasham.ali1979@gmail.com>
2020-09-23 00:46:01 +0500

    button add and contact page linkage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -202,6 +202,10 @@ class Header extends Component {
           </div>
         </Navbar>
         <div className={styles.FloatingMenu}>
+          <div>
+            <a href="/assessment">ASSESSMENT</a>
+          </div>
+
           {headerInfo.floatingmenu.map((item, index) => (
             <div key={index}>
               <a href={item.btnlink}>{item.btntext}</a>
