8c1099e41882c51abbca9c918a4f4fb9da70441e
EhtashamAli <ehtasham.ali1979@gmail.com>
2020-09-30 23:00:45 +0500

    promo bar removed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -80,7 +80,7 @@ class Header extends Component {
           expand="lg"
           className={`${styles.Navbar}`}
         >
-          {headerInfo.promoTopLine ? (
+          {/* {headerInfo.promoTopLine ? (
             <div className="promobar">
               <div className="container">
                 <div className="row ">
@@ -93,7 +93,7 @@ class Header extends Component {
                 </div>
               </div>
             </div>
-          ) : null}
+          ) : null} */}
 
           <div className={styles.headerTopLine}>
             <div className={`container ${styles.container}`}>
