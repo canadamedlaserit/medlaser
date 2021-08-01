4203babebda1ccd3e7697e887118015545e116bc
vosaul <v986963@gmail.com>
2021-08-01 11:36:33 +0300

    minor change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -133,7 +133,7 @@ class Header extends Component {
                   <Link
                     to="/choose"
                     state={{
-                      modal: true,
+                      modal: false,
                     }}
                     className={`btn btn-red ${styles.Btn}`}
                   >
